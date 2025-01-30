4fa89d37adde9e43714e990de705c9aa5db5afe7
hamodyArc <mohamad.alaktaa@elev.ga.ntig.se>
2025-01-30 13:22:03 +0100

    hello

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+def Weather_controller(location):
+    url = f"http://api.weatherapi.com/v1/current.json?key=3adbea022f0d4d778f1230311242306&q={location}"
+    req = requests.get(url)
+    temp = req.json()["current"]["temp_c"]
+    print(temp)
+location = input("Enter location: ")
+Weather_controller(location)
